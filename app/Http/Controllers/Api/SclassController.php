03f9337356a724a44ac53f8f34e106a688c5003a
Taghlaoui Oussama <oussamataghlaoui20@gmail.com>
2023-01-29 13:56:54 +0100

    RestAPI MCR,First Route and Method

--- app/Http/Controllers/Api/SclassController.php
+++ app/Http/Controllers/Api/SclassController.php
@@ -3,9 +3,13 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Sclass;
 use Illuminate\Http\Request;
 
 class SclassController extends Controller
 {
-    //
+    public function Index(){
+        $sclass = Sclass::latest()->get();
+        return response()->json($sclass);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\SclassController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/class', [SclassController::class, 'Index']);
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
