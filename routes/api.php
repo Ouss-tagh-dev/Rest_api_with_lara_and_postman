d8256c5623521fc04a4bd92a94fb77d2859dc5f5
Taghlaoui Oussama <oussamataghlaoui20@gmail.com>
2023-01-29 20:09:04 +0100

    RestAPI Subject CRUD

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\SclassController;
+use App\Http\Controllers\Api\SubjectController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,12 +15,29 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+// Student Class Routes
 Route::get('/class', [SclassController::class, 'Index']);
 Route::post('/class/store', [SclassController::class, 'Store']);
 Route::get('/class/edit/{id}', [SclassController::class, 'Edit']);
 Route::post('/class/update/{id}', [SclassController::class, 'Update']);
 Route::get('/class/delete/{id}', [SclassController::class, 'Delete']);
+
+
+// Student Subject Routes
+Route::get('/subject', [SubjectController::class, 'Index']);
+Route::post('/subject/store', [SubjectController::class, 'Store']);
+Route::get('/subject/edit/{id}', [SubjectController::class, 'Edit']);
+Route::post('/subject/update/{id}', [SubjectController::class, 'Update']);
+Route::get('/subject/delete/{id}', [SubjectController::class, 'Delete']);
+
+
+
+
+
+
+
+
+
 
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
